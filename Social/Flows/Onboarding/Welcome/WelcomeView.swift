1899543ee15548226369412aa6c4274e63f4230a
Christian Ampe <christianampe.work@gmail.com>
2020-06-30 18:50:40 -0700

    Fixed breaking changes and added buttons to the WelcomeView.

--- Social/Flows/Onboarding/Welcome/WelcomeView.swift
+++ Social/Flows/Onboarding/Welcome/WelcomeView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Spacer()
+                Button("Log In") {
+                    
+                }
+                Button("Sign Up") {
+                    
+                }
+            }
+            .navigationTitle("Welcome")
+        }
     }
 }
 
