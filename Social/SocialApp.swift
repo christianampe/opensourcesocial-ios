db419cfb937bd8decc5e970ebc2e50983e9d7bdc
Christian Ampe <christianampe.work@gmail.com>
2020-06-30 18:30:02 -0700

    Initial Commit

---
+++ Social/SocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialApp.swift
+//  Social
+//
+//  Created by Christian Ampe on 6/30/20.
+//
+
+import SwiftUI
+
+@main
+struct SocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
