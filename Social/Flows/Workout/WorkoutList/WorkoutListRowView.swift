752060732e0c484f66b73c14ed73603696074f15
eamiller34 <eamiller34@gmail.com>
2020-07-06 19:40:58 -0700

    Adds Workout Detail Destination Link

--- Social/Flows/Workout/WorkoutList/WorkoutListRowView.swift
+++ Social/Flows/Workout/WorkoutList/WorkoutListRowView.swift
@@ -12,14 +12,16 @@ struct WorkoutListRowView: View {
     
     //MARK: Turning on the observed object on causes errors in preview
     //MARK: How do you access a variable in a nested struct? (i.e. workoutTrackerVM.workoutTracker.workouts.workoutTitle)
-    //@ObservedObject var workoutTrackerVM: WorkoutListRowViewModel
+    //@ObservedObject var workoutRowVM: WorkoutListRowViewModel
     
     var body: some View {
         VStack (alignment: .leading){
-            Text("Workout 1")
-            Text("April 20, 2020")
-                .font(.caption)
-        }.padding(.vertical, 10)
+            NavigationLink(destination: WorkoutDetailView(workout: workoutRowVM.workout)){
+                Text("Workout 1")
+                Text("April 20, 2020")
+                    .font(.caption)
+            }.padding(.vertical, 10)
+        }
     }
 }
 
